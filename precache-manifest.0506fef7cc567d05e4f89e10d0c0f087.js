8df8575f8ac8d5e18776de3e4ae2d086d6040d83
FreezeNow <nonebreezy@foxmail.com>
2020-12-24 19:15:51 +0800

    deploy

---
+++ precache-manifest.0506fef7cc567d05e4f89e10d0c0f087.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1e29ffe736583f233dd71f4f8824d7",
+    "url": "CNAME"
+  },
+  {
+    "revision": "e099a214d82791d34ff3",
+    "url": "css/app.8da4147d.css"
+  },
+  {
+    "revision": "59e6bc2c65bf1df3c8e9",
+    "url": "css/chunk-60692120.d89d8dc6.css"
+  },
+  {
+    "revision": "6471e9792e09a94e94fd",
+    "url": "css/chunk-vendors.07f96dc8.css"
+  },
+  {
+    "revision": "c8e98847be9daaf7e3c779d01731e977",
+    "url": "img/CocoaBeans1.c8e98847.jpg"
+  },
+  {
+    "revision": "fe0bf855f332a6652272dc9302313a70",
+    "url": "img/CocoaFruits.fe0bf855.jpg"
+  },
+  {
+    "revision": "af295fa4e715eb59d00a14597bb1c9f6",
+    "url": "img/CocoaPowder.af295fa4.jpg"
+  },
+  {
+    "revision": "63b02d9c7dab93a89c10dbdf16c0e8ec",
+    "url": "img/MonerCocoa1.63b02d9c.png"
+  },
+  {
+    "revision": "7c06a24071e8f4ac295e9597c31fcdf3",
+    "url": "img/bg_05.7c06a240.jpg"
+  },
+  {
+    "revision": "88dc14ef4af7b884e39c4b6c12003d31",
+    "url": "img/bg_1.88dc14ef.png"
+  },
+  {
+    "revision": "cbcce027f75586bcc76f354bb0b41d85",
+    "url": "img/bg_10.cbcce027.jpg"
+  },
+  {
+    "revision": "f460eac34017b8bfefb74a65d5a78703",
+    "url": "img/bg_12.f460eac3.jpg"
+  },
+  {
+    "revision": "e532a63a0ae9c946fc147ed9f859da92",
+    "url": "img/bg_15.e532a63a.jpg"
+  },
+  {
+    "revision": "4efc0ec7fe0e47df6522b03f5b1499c9",
+    "url": "img/bg_18.4efc0ec7.jpg"
+  },
+  {
+    "revision": "bf4f2920f4576ad18016f51180e12a12",
+    "url": "img/bg_1_en.bf4f2920.png"
+  },
+  {
+    "revision": "19967957e5d2fb961ab0c015e46cafde",
+    "url": "img/companyfooter.19967957.jpg"
+  },
+  {
+    "revision": "50c9dcb9d3da43596bdde243b509c664",
+    "url": "img/footerimg.50c9dcb9.jpg"
+  },
+  {
+    "revision": "1f1672dab60e1e3032e2fd168037d13d",
+    "url": "index.html"
+  },
+  {
+    "revision": "e099a214d82791d34ff3",
+    "url": "js/app.31d1ea6e.js"
+  },
+  {
+    "revision": "59e6bc2c65bf1df3c8e9",
+    "url": "js/chunk-60692120.2e3a6d5d.js"
+  },
+  {
+    "revision": "6471e9792e09a94e94fd",
+    "url": "js/chunk-vendors.cff2c087.js"
+  },
+  {
+    "revision": "79e54b69450b79e88b142a1ffeef8a55",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
